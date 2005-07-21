826c0b06cfa2e479a5bbc170259a6f0f0c630c55
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-07-21 15:37:13 +0000

    * .classpath 	o add junit.jar * add dummy test-class

---
+++ src/test/de/ingrid/ibus/v01/DispatcherTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 1997-2005 by media style GmbH
+ * 
+ * $Source: DispatcherTest.java,v $
+ */
+
+package de.ingrid.ibus.v01;
+
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ * created on 21.07.2005 <p>
+ *
+ * Test class for the feature Dispatcher
+ *
+ * @author Marko Bauhardt
+ */
+public class DispatcherTest extends TestCase {
+
+    public void testReturnOfResults() {
+
+        fail("INGRID-19 not yet implemented");
+    }
+}
