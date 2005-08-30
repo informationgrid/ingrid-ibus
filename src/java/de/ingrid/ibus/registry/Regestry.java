2cdbb6da8b882b2104de641edb22cc6b35456cdb
hs <hs@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-08-30 13:28:35 +0000

    added implementation of method stubs for registration and querying iplugs

--- src/java/de/ingrid/ibus/registry/Regestry.java
+++ src/java/de/ingrid/ibus/registry/Regestry.java
@@ -6,25 +6,76 @@
 
 package de.ingrid.ibus.registry;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
 import de.ingrid.iplug.IIPlug;
 import de.ingrid.utils.IngridQuey;
 
+/**
+ * 
+ * created on 21.07.2005 <p>
+ *
+ * @author hs
+ */
+
 public class Regestry {
-
+    private Hashtable fRegisteredIPlugs = new Hashtable();
+    
+    /**
+     * Registers the given <code>IIPlug</code> instance.
+     * @param plug The <code>IIPlug</code> instance to register.
+     */
     public void addIPlug(IIPlug plug) {
-        // TODO Auto-generated method stub
-        
+        this.fRegisteredIPlugs.put(plug.getId(), plug);
     }
 
+    /**
+     * Allows access to all registered <code>IIplug</code> instances. 
+     * @param id The id of the registered <code>IIPlug</code> instance.
+     * @return null if no <code>IIPlug</code> instance is registered.
+     */
     public IIPlug getIPlug(String id) {
-        return null;
-        // TODO Auto-generated method stub
-        
+        return (IIPlug) this.fRegisteredIPlugs.get(id);
     }
 
-    public IIPlug[] getIPlugsForQuery(IngridQuey quey) {
-        // TODO Auto-generated method stub
-        return null;
+    /**
+     * 
+     * @param query
+     * @return All registered <code>IIPlug</code> instances which supports data fields which in the given query contains.       
+     */
+    public IIPlug[] getIPlugsForQuery(IngridQuey query) {
+        final List iPlugList = new ArrayList();
+        Iterator iterator = this.fRegisteredIPlugs.values().iterator();
+        IIPlug plug = null;
+        while (iterator.hasNext()) {
+            plug = (IIPlug) iterator.next();
+            if(checkForDataFields(query, plug.getFields())){
+                iPlugList.add(plug);
+            }
+        }
+        return (IIPlug[]) iPlugList.toArray(new IIPlug[iPlugList.size()]);
+    }
+    
+    
+    /**
+     * Checks for occurrence of datafields in the query. 
+     * @param ingridQuery The query to check for coccurrence of the given datafields.
+     * @param dataFields The fields to check for occurrence in the given query.
+     * @return true if one of the datafields occurs in the given query.  
+     */
+    private static boolean checkForDataFields(IngridQuey ingridQuery, String[] dataFields){
+        final Enumeration fields = ingridQuery.getFields();
+        final List dataFieldList = Arrays.asList(dataFields);
+        while (fields.hasMoreElements()) {
+            if(dataFieldList.contains(fields.nextElement())){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
