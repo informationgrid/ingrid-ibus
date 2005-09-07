45ceb8151cc4a700f25c56a7ac7f50257456cefd
sg <sg@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-09-07 15:55:21 +0000

    add emthy objects to continue later..

---
+++ src/test/de/ingrid/ibus/IBusTest.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 1997-2005 by media style GmbH
+ * 
+ * $Source: /cvs/asp-search/src/java/com/ms/aspsearch/PermissionDeniedException.java,v $
+ */
+
+package de.ingrid.ibus;
+
+import junit.framework.TestCase;
+
+public class IBusTest extends TestCase {
+
+    public void testSendQueries() throws Exception {
+        fail("to do");
+    }
+
+}
