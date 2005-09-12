326c79a7ae764cf23d8ca08bcc3298fa3e509fc0
hs <hs@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-09-12 16:44:22 +0000

    added tests as feature tests for parsing queries

--- src/test/de/ingrid/ibus/registry/SyntaxInterpreterTest.java
+++ src/test/de/ingrid/ibus/registry/SyntaxInterpreterTest.java
@@ -10,11 +10,59 @@ import java.io.StringReader;
 
 import junit.framework.TestCase;
 import de.ingrid.iplug.IIPlug;
+import de.ingrid.utils.FieldQuery;
 import de.ingrid.utils.IngridQuery;
+import de.ingrid.utils.TermQuery;
+import de.ingrid.utils.queryparser.ParseException;
 import de.ingrid.utils.queryparser.QueryStringParser;
 
 public class SyntaxInterpreterTest extends TestCase {
 
+    /**
+     * test for INGRID-36
+     * @throws ParseException
+     */
+    
+    public void testParseQuery() throws ParseException{
+        testSimple();
+        testFieldQuery();
+        testBooleanQuery();
+        
+    }
+    
+    private void testSimple() throws ParseException{
+        IngridQuery query = SyntaxInterpreter.parseQuery("halle saale");
+        boolean containsHalle = false;
+        boolean containsSaale = false;
+        TermQuery[] terms = query.getTerms();
+        for (int i = 0; i < terms.length; i++) {
+            if("halle".equals(terms[i].getTerm())){
+                containsHalle = true;
+            }
+            if("saale".equals(terms[i].getTerm())){
+                containsSaale = true;
+            }
+        }
+        assertTrue(containsHalle);
+        assertTrue(containsSaale);
+    }
+    
+    private void testFieldQuery() throws ParseException{
+        IngridQuery query = SyntaxInterpreter.parseQuery("field1:value1 field2:value2 term");
+        FieldQuery[] fields = query.getFields();
+        for (int i = 1; i <= fields.length; i++) {
+            FieldQuery field = fields[i-1];
+            assertEquals("field" + i, field.getFieldName());
+            assertEquals("value" + i, field.getFieldValue());
+        }
+    }
+    
+    private void testBooleanQuery() throws ParseException{
+        IngridQuery query = SyntaxInterpreter.parseQuery("term1 OR term2");
+        assertEquals(2, query.getTerms().length);
+        assertEquals(IngridQuery.OR, query.getTerms()[1].getOperation());
+    }
+    
     /**
      * 
      * @throws Exception
