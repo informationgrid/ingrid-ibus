22537eaa4d70fb9a2725eab6c4e5918e11f47bcc
sg <sg@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-09-19 16:03:28 +0000

    work in progress

--- src/test/de/ingrid/ibus/registry/SyntaxInterpreterTest.java
+++ src/test/de/ingrid/ibus/registry/SyntaxInterpreterTest.java
@@ -9,60 +9,12 @@ package de.ingrid.ibus.registry;
 import java.io.StringReader;
 
 import junit.framework.TestCase;
-import de.ingrid.iplug.IIPlug;
-import de.ingrid.utils.FieldQuery;
-import de.ingrid.utils.IngridQuery;
-import de.ingrid.utils.TermQuery;
-import de.ingrid.utils.queryparser.ParseException;
+import de.ingrid.iplug.PlugDescription;
+import de.ingrid.utils.query.IngridQuery;
 import de.ingrid.utils.queryparser.QueryStringParser;
 
 public class SyntaxInterpreterTest extends TestCase {
 
-    /**
-     * test for INGRID-36
-     * @throws ParseException
-     */
-    
-    public void testParseQuery() throws ParseException{
-        testSimple();
-        testFieldQuery();
-        testBooleanQuery();
-        
-    }
-    
-    private void testSimple() throws ParseException{
-        IngridQuery query = SyntaxInterpreter.parseQuery("halle saale");
-        boolean containsHalle = false;
-        boolean containsSaale = false;
-        TermQuery[] terms = query.getTerms();
-        for (int i = 0; i < terms.length; i++) {
-            if("halle".equals(terms[i].getTerm())){
-                containsHalle = true;
-            }
-            if("saale".equals(terms[i].getTerm())){
-                containsSaale = true;
-            }
-        }
-        assertTrue(containsHalle);
-        assertTrue(containsSaale);
-    }
-    
-    private void testFieldQuery() throws ParseException{
-        IngridQuery query = SyntaxInterpreter.parseQuery("field1:value1 field2:value2 term");
-        FieldQuery[] fields = query.getFields();
-        for (int i = 1; i <= fields.length; i++) {
-            FieldQuery field = fields[i-1];
-            assertEquals("field" + i, field.getFieldName());
-            assertEquals("value" + i, field.getFieldValue());
-        }
-    }
-    
-    private void testBooleanQuery() throws ParseException{
-        IngridQuery query = SyntaxInterpreter.parseQuery("term1 OR term2");
-        assertEquals(2, query.getTerms().length);
-        assertEquals(IngridQuery.OR, query.getTerms()[1].getOperation());
-    }
-    
     /**
      * 
      * @throws Exception
@@ -71,19 +23,21 @@ public class SyntaxInterpreterTest extends TestCase {
         Regestry regestry = new Regestry();
         for (int i = 0; i < 10; i++) {
             String dummy = "" + i;
-            regestry.addIPlug(new DummyIPlug(dummy, new String[] { dummy }));
+            PlugDescription description = new PlugDescription();
+            description.addField(dummy);
+            regestry.addIPlug(description);
         }
         for (int i = 0; i < 10; i++) {
             String dummy = "" + i;
             QueryStringParser parser = new QueryStringParser(new StringReader(dummy + ":" + dummy));
             IngridQuery query = parser.parse();
-            IIPlug[] plugsForQuery = SyntaxInterpreter.getIPlugsForQuery(query, regestry);
+            PlugDescription[] plugsForQuery = SyntaxInterpreter.getIPlugsForQuery(query, regestry);
             assertEquals(1, plugsForQuery.length);
         }
-    
+
         QueryStringParser parser = new QueryStringParser(new StringReader("a simple Query"));
         IngridQuery query = parser.parse();
-        assertEquals(10,  SyntaxInterpreter.getIPlugsForQuery(query, regestry).length);
+        assertEquals(10, SyntaxInterpreter.getIPlugsForQuery(query, regestry).length);
     }
 
 }
