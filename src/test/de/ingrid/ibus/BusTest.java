64bd3d7acf9c2723e3e56b3282024e8300407b64
hz <hz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-10-19 15:21:58 +0000

    fix

--- src/test/de/ingrid/ibus/BusTest.java
+++ src/test/de/ingrid/ibus/BusTest.java
@@ -12,19 +12,32 @@ import de.ingrid.utils.IngridDocument;
 import de.ingrid.utils.query.IngridQuery;
 import de.ingrid.utils.queryparser.QueryStringParser;
 
+/**
+ * BusTest
+ * 
+ * <p/>created on 07.09.2005
+ * 
+ * @version $Revision: $
+ * @author sg
+ * @author $Author jz ${lastedit}
+ * 
+ */
 public class BusTest extends TestCase {
 
+    /**
+     * @throws Exception
+     */
     public void testSendQueries() throws Exception {
         Bus bus = new Bus(new DummyProxyFactory());
-        
+
         for (int i = 0; i < 3; i++) {
             PlugDescription plug = new PlugDescription();
+            plug.setPlugId("" + i);
             bus.getIPlugRegestry().addIPlug(plug);
         }
 
-        
         IngridQuery query = QueryStringParser.parse("fische ort:halle");
-        IngridDocument[] documents = bus.search(query, 10, 1,  Integer.MAX_VALUE, 1000);
+        IngridDocument[] documents = bus.search(query, 10, 1, Integer.MAX_VALUE, 1000);
         assertEquals(3, documents.length);
 
     }
