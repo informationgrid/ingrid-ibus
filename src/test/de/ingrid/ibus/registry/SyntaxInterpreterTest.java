cb2102be9e86fb253b3b900de90c486c9c06ec51
hz <hz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-10-20 10:20:20 +0000

    moved some test methods to the feature-test (cause they are testing QueryStringParser from ingrid-util)

--- src/test/de/ingrid/ibus/registry/SyntaxInterpreterTest.java
+++ src/test/de/ingrid/ibus/registry/SyntaxInterpreterTest.java
@@ -10,82 +10,66 @@ import java.io.StringReader;
 
 import junit.framework.TestCase;
 import de.ingrid.iplug.PlugDescription;
-import de.ingrid.utils.query.FieldQuery;
 import de.ingrid.utils.query.IngridQuery;
-import de.ingrid.utils.query.TermQuery;
-import de.ingrid.utils.queryparser.ParseException;
 import de.ingrid.utils.queryparser.QueryStringParser;
 
+/**
+ * SyntaxInterpreterTest
+ * 
+ * <p/>created on 07.09.2005
+ * 
+ * @version $Revision: $
+ * @author sg
+ * @author $Author: ${lastedit}
+ * 
+ */
 public class SyntaxInterpreterTest extends TestCase {
 
-    /**
-     * test for INGRID-36
-     * @throws ParseException
-     */
-    
-    public void testParseQuery() throws ParseException{
-        testSimple();
-        testFieldQuery();
-        testBooleanQuery();
-        
-    }
-    
-    public void testSimple() throws ParseException{
-        IngridQuery query = QueryStringParser.parse("halle saale");
-        boolean containsHalle = false;
-        boolean containsSaale = false;
-        TermQuery[] terms = query.getTerms();
-        for (int i = 0; i < terms.length; i++) {
-            if("halle".equals(terms[i].getTerm())){
-                containsHalle = true;
-            }
-            if("saale".equals(terms[i].getTerm())){
-                containsSaale = true;
-            }
-        }
-        assertTrue(containsHalle);
-        assertTrue(containsSaale);
-    }
-    
-    public void testFieldQuery() throws ParseException{
-        IngridQuery query = QueryStringParser.parse("field1:value1 field2:value2 term");
-        FieldQuery[] fields = query.getFields();
-        for (int i = 1; i <= fields.length; i++) {
-            FieldQuery field = fields[i-1];
-            assertEquals("field" + i, field.getFieldName());
-            assertEquals("value" + i, field.getFieldValue());
-        }
-    }
-    
-    public void testBooleanQuery() throws ParseException, de.ingrid.utils.queryparser.ParseException{
-        IngridQuery query = QueryStringParser.parse("term1 OR term2");
-        assertEquals(2, query.getTerms().length);
-        assertEquals(IngridQuery.OR, query.getTerms()[1].getOperation());
-    }
     /**
      * 
      * @throws Exception
      */
     public void testGetIplugsForQuery() throws Exception {
-        Registry regestry = new Registry(1000000);
+        Registry registry = new Registry(1000000);
         for (int i = 0; i < 10; i++) {
             String dummy = "" + i;
             PlugDescription description = new PlugDescription();
             description.addField(dummy);
             description.setPlugId(dummy);
-            regestry.addIPlug(description);
+            registry.addIPlug(description);
         }
         for (int i = 0; i < 10; i++) {
             String dummy = "" + i;
             QueryStringParser parser = new QueryStringParser(new StringReader(dummy + ":" + dummy));
             IngridQuery query = parser.parse();
-            PlugDescription[] plugsForQuery = SyntaxInterpreter.getIPlugsForQuery(query, regestry);
+            PlugDescription[] plugsForQuery = SyntaxInterpreter.getIPlugsForQuery(query, registry);
             assertEquals(1, plugsForQuery.length);
         }
 
         QueryStringParser parser = new QueryStringParser(new StringReader("a simple Query"));
         IngridQuery query = parser.parse();
-        assertEquals(10, SyntaxInterpreter.getIPlugsForQuery(query, regestry).length);
+        assertEquals(10, SyntaxInterpreter.getIPlugsForQuery(query, registry).length);
     }
 
+    /**
+     * @throws Exception
+     */
+    public void testGetIplugsForQuery_Fields() throws Exception {
+        // TODO implement
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetIplugsForQuery_DataTypes() throws Exception {
+        // TODO implement
+
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetIplugsForQuery_FieldsAndDataTypes() throws Exception {
+        // TODO implement
+    }
 }
--- src/test/de/ingrid/ibus/v01/SyntaxInterpreterTest.java
+++ src/test/de/ingrid/ibus/v01/SyntaxInterpreterTest.java
@@ -4,36 +4,87 @@
  * $Source: DispatcherTest.java,v $
  */
 package de.ingrid.ibus.v01;
+
 import junit.framework.TestCase;
+import de.ingrid.utils.query.FieldQuery;
+import de.ingrid.utils.query.IngridQuery;
+import de.ingrid.utils.query.TermQuery;
+import de.ingrid.utils.queryparser.ParseException;
+import de.ingrid.utils.queryparser.QueryStringParser;
 
 /**
- * Test for syntax-interpreter-feature (INGRID-1).
- * created on 21.07.2005 <p>
- *
+ * Test for syntax-interpreter-feature (INGRID-1). created on 21.07.2005
+ * <p>
+ * 
  * @author hs
  */
 public class SyntaxInterpreterTest extends TestCase {
     private static de.ingrid.ibus.registry.SyntaxInterpreterTest interpreterTest = new de.ingrid.ibus.registry.SyntaxInterpreterTest();
-    
+
     /**
      * test for feature INGRID-36
-     * @throws Exception 
+     * 
+     * @throws Exception
      */
     public void testTranslateToQuerySyntax() throws Exception {
-       interpreterTest.testParseQuery(); 
+        // tests functionality from QueryStringParser from ingrid-utils
+        checkParseSimple();
+        checkParseFieldQuery();
+        checkParseBooleanQuery();
     }
+
     /**
-     * test for feature INGRID-37  
-     * @throws Exception 
+     * test for feature INGRID-37
+     * 
+     * @throws Exception
      */
-    public void testFindAddressedDataSources() throws Exception{
+    public void testFindAddressedDataSources() throws Exception {
         interpreterTest.testGetIplugsForQuery();
     }
+
     /**
      * test for feature INGRID-38
-     * @throws Exception 
+     * 
+     * @throws Exception
      */
     public void testFindDataFields() throws Exception {
-        interpreterTest.testGetIplugsForQuery();
+        interpreterTest.testGetIplugsForQuery_Fields();
+        interpreterTest.testGetIplugsForQuery_DataTypes();
+        interpreterTest.testGetIplugsForQuery_FieldsAndDataTypes();
+    }
+
+    // ------------private------------------
+
+    private void checkParseSimple() throws ParseException {
+        IngridQuery query = QueryStringParser.parse("halle saale");
+        boolean containsHalle = false;
+        boolean containsSaale = false;
+        TermQuery[] terms = query.getTerms();
+        for (int i = 0; i < terms.length; i++) {
+            if ("halle".equals(terms[i].getTerm())) {
+                containsHalle = true;
+            }
+            if ("saale".equals(terms[i].getTerm())) {
+                containsSaale = true;
+            }
+        }
+        assertTrue(containsHalle);
+        assertTrue(containsSaale);
+    }
+
+    private void checkParseFieldQuery() throws ParseException {
+        IngridQuery query = QueryStringParser.parse("field1:value1 field2:value2 term");
+        FieldQuery[] fields = query.getFields();
+        for (int i = 1; i <= fields.length; i++) {
+            FieldQuery field = fields[i - 1];
+            assertEquals("field" + i, field.getFieldName());
+            assertEquals("value" + i, field.getFieldValue());
+        }
+    }
+
+    private void checkParseBooleanQuery() throws ParseException, de.ingrid.utils.queryparser.ParseException {
+        IngridQuery query = QueryStringParser.parse("term1 OR term2");
+        assertEquals(2, query.getTerms().length);
+        assertEquals(IngridQuery.OR, query.getTerms()[1].getOperation());
     }
 }
