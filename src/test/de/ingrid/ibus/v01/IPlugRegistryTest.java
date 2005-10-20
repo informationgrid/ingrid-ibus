4e67d74d0f61c9009c2499b99389d02ec7614ac9
hz <hz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-10-20 10:16:37 +0000

    implement testStoreIndexStructuresTemporary

--- src/test/de/ingrid/ibus/v01/IPlugRegistryTest.java
+++ src/test/de/ingrid/ibus/v01/IPlugRegistryTest.java
@@ -4,56 +4,67 @@
  * $Source: DispatcherTest.java,v $
  */
 package de.ingrid.ibus.v01;
+
 import de.ingrid.ibus.registry.RegistryTest;
 import de.ingrid.ibus.registry.SyntaxInterpreterTest;
 import junit.framework.TestCase;
 
 /**
- * Test for iplug-registry-feature (INGRID-5).
- * created on 21.07.2005 <p>
- *
+ * Test for iplug-registry-feature (INGRID-5). created on 21.07.2005
+ * <p>
+ * 
  * @author hs
  */
 public class IPlugRegistryTest extends TestCase {
+
     /**
-     * 
+     * INGRID-28
      */
     public void testParseMetaData() {
         fail("INGRID-28 not yet implemented");
     }
+
     /**
+     * INGRID-29
      * 
+     * @throws Exception
      */
-    public void testStoreConnectionSettingsLocally() {
-        fail("INGRID-29 not yet implemented");
+    public void testStoreConnectionSettingsLocally() throws Exception {
+        new RegistryTest().testAddAndGet();
     }
+
     /**
-     * @throws Exception 
+     * INGRID-30
      * 
+     * @throws Exception
      */
     public void testStoreIndexStructures() throws Exception {
         new RegistryTest().testAddAndGet();
         new SyntaxInterpreterTest().testGetIplugsForQuery();
-        
-        
+
     }
+
     /**
+     * INGRID-31
      * 
+     * @throws Exception
      */
-    public void testStoreIndexStructuresTemporary() {
-        fail("INGRID-31 not yet implemented");
+    public void testStoreIndexStructuresTemporary() throws Exception {
+        new RegistryTest().testGetAllIPlugs();
     }
+
     /**
-     * test for INGRID-32
+     * INGRID-32
      * 
-     * @throws Exception 
+     * @throws Exception
      * 
      */
     public void testStoreDataInRAM() throws Exception {
         new RegistryTest().testAddAndGet();
     }
+
     /**
-     * 
+     * INGRID-33
      */
     public void testSearchAPI() {
         fail("INGRID-33 not yet implemented");
