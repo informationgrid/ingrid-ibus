c703d58108aa5023b4eedfdf7a1926a40abffbf3
hz <hz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-10-25 11:48:55 +0000

    o test processor pipe with statistic processors

--- src/test/de/ingrid/ibus/BusTest.java
+++ src/test/de/ingrid/ibus/BusTest.java
@@ -7,8 +7,18 @@
 package de.ingrid.ibus;
 
 import junit.framework.TestCase;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.ErrorHandler;
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggingEvent;
+
 import de.ingrid.iplug.PlugDescription;
 import de.ingrid.utils.IngridDocument;
+import de.ingrid.utils.processor.impl.StatisticPostProcessor;
+import de.ingrid.utils.processor.impl.StatisticPreProcessor;
 import de.ingrid.utils.query.IngridQuery;
 import de.ingrid.utils.queryparser.QueryStringParser;
 
@@ -24,22 +34,120 @@ import de.ingrid.utils.queryparser.QueryStringParser;
  */
 public class BusTest extends TestCase {
 
+    private Bus bus;
+
+    private PlugDescription[] plugDescriptions = new PlugDescription[3];
+    
     /**
-     * @throws Exception
+     * Call setUp for the feature tests
+     * @throws Exception 
+     * 
      */
-    public void testSendQueries() throws Exception {
-        Bus bus = new Bus(new DummyProxyFactory());
+    public BusTest() throws Exception {
+             setUp();
+    }
 
-        for (int i = 0; i < 3; i++) {
-            PlugDescription plug = new PlugDescription();
-            plug.setPlugId("" + i);
-            bus.getIPlugRegestry().addIPlug(plug);
+    protected void setUp() throws Exception {
+        this.bus = new Bus(new DummyProxyFactory());
+        for (int i = 0; i < this.plugDescriptions.length; i++) {
+            this.plugDescriptions[i] = new PlugDescription();
+            this.plugDescriptions[i].setPlugId("" + i);
+            this.bus.getIPlugRegestry().addIPlug(this.plugDescriptions[i]);
         }
-
-        IngridQuery query = QueryStringParser.parse("fische ort:halle");
-        IngridDocument[] documents = bus.search(query, 10, 1, Integer.MAX_VALUE, 1000);
-        assertEquals(3, documents.length);
 
     }
 
+    /**
+     * @throws Exception
+     */
+    public void testSearch() throws Exception {
+        IngridQuery query = QueryStringParser.parse("fische ort:halle");
+        IngridDocument[] documents = this.bus.search(query, 10, 1, Integer.MAX_VALUE, 1000);
+        assertEquals(this.plugDescriptions.length, documents.length);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testSearchWithStatisticPreProcessor() throws Exception {
+        TestAppender appender = new TestAppender();
+        Logger.getLogger(StatisticPreProcessor.class).addAppender(appender);
+        this.bus.getProccessorPipe().addPreProcessor(new StatisticPreProcessor());
+
+        IngridQuery query = QueryStringParser.parse("fische ort:halle");
+        this.bus.search(query, 10, 1, Integer.MAX_VALUE, 1000);
+        assertTrue(appender.fLastLogMessage.indexOf(query.toString()) != -1);
+    }
+    
+    /**
+     * @throws Exception
+     */
+    public void testSearchWithStatisticPostProcessor() throws Exception {
+        TestAppender appender = new TestAppender();
+        Logger.getLogger(StatisticPostProcessor.class).addAppender(appender);
+        this.bus.getProccessorPipe().addPostProcessor(new StatisticPostProcessor());
+
+        IngridQuery query = QueryStringParser.parse("fische ort:halle");
+        this.bus.search(query, 10, 1, Integer.MAX_VALUE, 1000);
+        assertTrue(appender.fLastLogMessage.indexOf(query.toString()) != -1);
+    }
+
+
+    private class TestAppender implements Appender {
+
+        /**
+         * 
+         */
+        public String fLastLogMessage;
+
+        public void addFilter(Filter arg0) {
+            //
+        }
+
+        public Filter getFilter() {
+            return null;
+        }
+
+        public void clearFilters() {
+            //
+        }
+
+        public void close() {
+            //
+        }
+
+        public void doAppend(LoggingEvent arg0) {
+            this.fLastLogMessage = arg0.getMessage().toString();
+
+        }
+
+        public String getName() {
+            return null;
+        }
+
+        public void setErrorHandler(ErrorHandler arg0) {
+            //
+        }
+
+        public ErrorHandler getErrorHandler() {
+            return null;
+        }
+
+        public void setLayout(Layout arg0) {
+            //
+        }
+
+        public Layout getLayout() {
+            return null;
+        }
+
+        public void setName(String arg0) {
+            //
+        }
+
+        public boolean requiresLayout() {
+            return false;
+        }
+
+    }
 }
