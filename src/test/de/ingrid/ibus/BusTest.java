ef6e30094e0aa43796a75681f3d9d3bff71a8858
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-01-17 17:29:54 +0000

    o remove the log dependency

--- src/test/de/ingrid/ibus/BusTest.java
+++ src/test/de/ingrid/ibus/BusTest.java
@@ -6,11 +6,7 @@
 
 package de.ingrid.ibus;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import junit.framework.TestCase;
-
 import de.ingrid.iplug.PlugDescription;
 import de.ingrid.utils.IngridHits;
 import de.ingrid.utils.processor.impl.StatisticPostProcessor;
