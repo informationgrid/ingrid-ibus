99ef8a44b5acdd7eb42812d775232f7deaee9b66
hz <hz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-03-21 13:12:05 +0000

    set class

--- src/scripts/start.bat
+++ src/scripts/start.bat
@@ -2,6 +2,8 @@
 
 set DIR=./
 if "%OS%" == "Windows_NT" set DIR=%~dp0%
+set CLASS=de.ingrid.ibus.BusServer
+echo call cmd /V:ON /C %DIR%starter.bat
 
-echo call cmd /V:ON /C %DIR%start_really.bat
-cmd /V:ON /C "%DIR%start_really.bat" %1 %2 %3 %4 %5
+rem cmd /V:ON /C "%DIR%starter.bat" %CLASS% 11111 11112 ibus.de 11113
+cmd /V:ON /C "%DIR%starter.bat" %CLASS% --descriptor %DIR%/conf/jxta.conf.xml --busurl
