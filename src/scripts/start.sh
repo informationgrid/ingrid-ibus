ba790c93757d669bfff9fd410a6e006c76845306
hz <hz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-03-21 13:55:15 +0000

    o set class to start o start starter.sh

---
+++ src/scripts/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# 
+# Environment Variables
+#
+#   INGRID_JAVA_HOME Overrides JAVA_HOME.
+#
+#   INGRID_HEAPSIZE  heap to use in mb, if not setted we use 1000.
+#
+#   INGRID_OPTS      addtional java runtime options
+
+
+CLASS=de.ingrid.ibus.BusServer
+sh starter.sh $CLASS --descriptor conf/jxta.conf.xml --busurl 
+# sh starter.sh $CLASS 11111 11112 ibus.de 11113
