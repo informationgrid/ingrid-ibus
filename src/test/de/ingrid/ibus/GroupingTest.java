73b21c577b1a8bbcd45901df491711be1728416a
sg <sg@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-04-06 14:59:25 +0000

    comment in simple test

--- src/test/de/ingrid/ibus/GroupingTest.java
+++ src/test/de/ingrid/ibus/GroupingTest.java
@@ -8,40 +8,40 @@ import junit.framework.TestCase;
 
 public class GroupingTest extends TestCase {
 
-//    private static final String ORGANISATION = "a organisation";
-//
-//    private Bus fBus;
-//
-//    private PlugDescription[] plugDescriptions = new PlugDescription[5];
-//
-//    protected void setUp() throws Exception {
-//
-//        this.fBus = new Bus(new DummyProxyFactory());
-//        for (int i = 0; i < this.plugDescriptions.length; i++) {
-//            this.plugDescriptions[i] = new PlugDescription();
-//            this.plugDescriptions[i].setPlugId("" + i);
-//            this.plugDescriptions[i].setOrganisation(ORGANISATION);
-//            this.fBus.getIPlugRegistry().addIPlug(this.plugDescriptions[i]);
-//        }
-//    }
-//
-//    public void testGrouping() throws Exception {
-//
-//        IngridQuery query = QueryStringParser.parse("aQuery grouped:" + IngridQuery.GROUPED_BY_ORGANISATION);
-//        int hitsPerPage = 10;
-//        int currentPage = 1;
-//        int length = 1000;
-//        int maxMilliseconds = 1000;
-//        IngridHits hits = fBus.search(query, hitsPerPage, currentPage, length, maxMilliseconds);
-//        assertEquals(5, hits.length());
-//        assertEquals(1, hits.getHits().length);
-//
-//        query = QueryStringParser.parse("aQuery grouped:" + IngridQuery.GROUPED_BY_PLUGID);
-//        hits = fBus.search(query, hitsPerPage, currentPage, length, maxMilliseconds);
-//        assertEquals(5, hits.length());
-//        assertEquals(5, hits.getHits().length);
-//
-//    }
+    private static final String ORGANISATION = "a organisation";
+
+    private Bus fBus;
+
+    private PlugDescription[] plugDescriptions = new PlugDescription[5];
+
+    protected void setUp() throws Exception {
+
+        this.fBus = new Bus(new DummyProxyFactory());
+        for (int i = 0; i < this.plugDescriptions.length; i++) {
+            this.plugDescriptions[i] = new PlugDescription();
+            this.plugDescriptions[i].setPlugId("" + i);
+            this.plugDescriptions[i].setOrganisation(ORGANISATION);
+            this.fBus.getIPlugRegistry().addIPlug(this.plugDescriptions[i]);
+        }
+    }
+
+    public void testGrouping() throws Exception {
+
+        IngridQuery query = QueryStringParser.parse("aQuery grouped:" + IngridQuery.GROUPED_BY_ORGANISATION);
+        int hitsPerPage = 10;
+        int currentPage = 1;
+        int length = 1000;
+        int maxMilliseconds = 1000;
+        IngridHits hits = fBus.search(query, hitsPerPage, currentPage, length, maxMilliseconds);
+        assertEquals(5, hits.length());
+        assertEquals(1, hits.getHits().length);
+
+        query = QueryStringParser.parse("aQuery grouped:" + IngridQuery.GROUPED_BY_PLUGID);
+        hits = fBus.search(query, hitsPerPage, currentPage, length, maxMilliseconds);
+        assertEquals(5, hits.length());
+        assertEquals(5, hits.getHits().length);
+
+    }
 
     public void testPaging() throws Exception {
         // setup
