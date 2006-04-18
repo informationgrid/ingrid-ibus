f1a0b697beb46850a95b43e57e2c13958549f49d
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-04-18 15:06:22 +0000

    trigger cc

--- src/java/de/ingrid/ibus/Bus.java
+++ src/java/de/ingrid/ibus/Bus.java
@@ -418,6 +418,7 @@ public class Bus extends Thread implements IBus {
 			}
 			requestHitList.add(hit);
 		}
+    
 		// send requests and collect response
 		Iterator iterator = hashMap.keySet().iterator();
 		IPlug plugProxy;
