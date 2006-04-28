b1f3c09a547e6c62be79e1f18ebec82d51f57062
hz <hz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-04-28 19:12:04 +0000

    added time information

--- src/java/de/ingrid/ibus/net/PlugQueryRequest.java
+++ src/java/de/ingrid/ibus/net/PlugQueryRequest.java
@@ -20,84 +20,79 @@ import de.ingrid.utils.query.IngridQuery;
  */
 public class PlugQueryRequest extends Thread {
 
-	private final static Log fLog = LogFactory.getLog(PlugQueryRequest.class);
+    private final static Log fLog = LogFactory.getLog(PlugQueryRequest.class);
 
-	private IngridQuery fQuery;
+    private IngridQuery fQuery;
 
-	private int fLength;
+    private int fLength;
 
-	private ResultSet fResultSet;
+    private ResultSet fResultSet;
 
-	private int fStart;
+    private int fStart;
 
-	private IPlug fIPlug;
+    private IPlug fIPlug;
 
-	private String fPlugId;
+    private String fPlugId;
 
-	private Registry fRegestry;
+    private Registry fRegestry;
 
-	/**
-	 * @param proxyFactory
-	 * @param plugDescription
-	 * @param query
-	 * @param start
-	 * @param length
-	 * @param plug
-	 * @throws Exception
-	 */
-	public PlugQueryRequest(IPlug plug, Registry registry, String plugId,
-			ResultSet resultSet, IngridQuery query, int start, int length)
-			throws Exception {
-		this.fIPlug = plug;
-		this.fRegestry = registry;
-		this.fPlugId = plugId;
-		this.fResultSet = resultSet;
-		this.fQuery = query;
-		this.fStart = start;
-		this.fLength = length;
+    /**
+     * @param query
+     * @param start
+     * @param length
+     * @param plug
+     * @param registry
+     * @param plugId
+     * @param resultSet
+     * @throws Exception
+     */
+    public PlugQueryRequest(IPlug plug, Registry registry, String plugId, ResultSet resultSet, IngridQuery query,
+            int start, int length) throws Exception {
+        this.fIPlug = plug;
+        this.fRegestry = registry;
+        this.fPlugId = plugId;
+        this.fResultSet = resultSet;
+        this.fQuery = query;
+        this.fStart = start;
+        this.fLength = length;
 
-	}
+    }
 
-	/**
-	 * @see java.lang.Thread#run()
-	 */
-	public void run() {
-		
-		try {
-			if (fLog.isDebugEnabled()) {
-				fLog.debug("Search in IPlug " + this.fPlugId + " ...");
-			}
-			IngridHits hits = this.fIPlug.search(this.fQuery, this.fStart,
-					this.fLength);
+    /**
+     * @see java.lang.Thread#run()
+     */
+    public void run() {
+        long time = 0 ;
+        try {
+            if (fLog.isDebugEnabled()) {
+                fLog.debug("Search in IPlug " + this.fPlugId + " ...");
+                time = System.currentTimeMillis();
+            }
+            IngridHits hits = this.fIPlug.search(this.fQuery, this.fStart, this.fLength);
 
-			if (fLog.isDebugEnabled()) {
-				fLog.debug("adding results from: " + fPlugId + " size: "
-						+ hits.length());
-			}
-			this.fResultSet.add(hits);
-
-		} catch (Exception e) {
-			fLog.error(
-					"(REMOVING IPLUG!) Could not retrieve query result from IPlug: "
-							+ this.fPlugId, e);
-			fRegestry.removePlugFromCache(fPlugId);
-
-		} finally {
-			if ( this.fResultSet !=null ) {
-                synchronized (fResultSet) {
-                    this.fResultSet.resultsAdded();    
+            if (fLog.isDebugEnabled()) {
+                fLog.debug("adding results from: " + this.fPlugId + " size: " + hits.length() + " time: "
+                        + (System.currentTimeMillis() - time) + " ms");
+            }
+            this.fResultSet.add(hits);
+        } catch (Exception e) {
+            fLog.error("(REMOVING IPLUG!) Could not retrieve query result from IPlug: " + this.fPlugId, e);
+            this.fRegestry.removePlugFromCache(this.fPlugId);
+        } finally {
+            if (this.fResultSet != null) {
+                synchronized (this.fResultSet) {
+                    this.fResultSet.resultsAdded();
                 }
-			} else {
-				fLog.error("No ResultSet set where IPlug " + this.fPlugId
-						+ " can sent its completion.");
-			}
-		}
-	}
+            } else {
+                fLog.error("No ResultSet set where IPlug " + this.fPlugId + " can sent its completion.");
+            }
+        }
+    }
 
-	/**
-	 * @return The IPlug itself.
-	 */
-	public IPlug getIPlug() {
-		return this.fIPlug;
-	}
+    /**
+     * @return The IPlug itself.
+     */
+    public IPlug getIPlug() {
+        return this.fIPlug;
+    }
 }
