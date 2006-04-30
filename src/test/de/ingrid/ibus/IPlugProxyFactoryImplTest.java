907cfb55df057a9b0b1fb1e553b5545f5a7063ff
hz <hz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-04-30 15:34:56 +0000

    setRecordLoader

--- src/test/de/ingrid/ibus/IPlugProxyFactoryImplTest.java
+++ src/test/de/ingrid/ibus/IPlugProxyFactoryImplTest.java
@@ -48,6 +48,7 @@ public class IPlugProxyFactoryImplTest extends TestCase {
             this.plugDescriptions[i].setProxyServiceURL("" + i);
             this.plugDescriptions[i].setProxyServiceURL(AddressUtil.getWetagURL("localhost", 60006 + i));
             this.plugDescriptions[i].setIPlugClass(DummyIPlug.class.getName());
+            this.plugDescriptions[i].setRecordLoader(false);
             this.plugDescriptions[i].addField("ort");
             this.fBus.getIPlugRegistry().addIPlug(this.plugDescriptions[i]);
         }
