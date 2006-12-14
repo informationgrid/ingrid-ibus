342448c44ecce02b4d391de95ead0ba943c99d27
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-12-14 13:43:02 +0000

    change start.sh script

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -116,7 +116,7 @@ startIplug()
   CLASS=de.ingrid.ibus.BusServer
   
   # run it
-  exec nohup "$JAVA" $JAVA_HEAP_MAX $INGRID_OPTS -classpath "$CLASSPATH" $CLASS --descriptor conf/jxta.properties --adminpassword --adminport --busurl 
+  exec nohup "$JAVA" $JAVA_HEAP_MAX $INGRID_OPTS -classpath "$CLASSPATH" $CLASS --descriptor conf/jxta.properties --adminpassword --adminport --busurl > console.log &
   
   echo "ingrid component ($INGRID_HOME) started."
   echo $! > $PID
