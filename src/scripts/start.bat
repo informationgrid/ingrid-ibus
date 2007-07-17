544c25fae70f0ccd9dc41fc8e2028cd8d8082624
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2007-07-17 16:18:10 +0000

    not necessary

--- src/scripts/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set DIR=./
-if "%OS%" == "Windows_NT" set DIR=%~dp0%
-set CLASS=de.ingrid.ibus.BusServer
-echo call cmd /V:ON /C %DIR%starter.bat
-
-cmd /V:ON /C "%DIR%starter.bat" %CLASS% --descriptor %DIR%/conf/communication.properties --adminpassword --adminport --busurl
--- src/scripts/update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set DIR=./
-if "%OS%" == "Windows_NT" set DIR=%~dp0%
-set CLASS=de.ingrid.update.Update
-
-echo call cmd /V:ON /C %DIR%starter.bat
-cmd /V:ON /C "%DIR%starter.bat" %CLASS% %1 %2 %3 %4 %5 %6 %7 %8 %9 %10
