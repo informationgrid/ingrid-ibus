34949dbc04c1e3783e89d92068e8141b326d9d23
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2013-04-15 14:53:10 +0000

    - update test for query pre processor (add property file to be able to test a real scenario)

--- src/test/java/de/ingrid/ibus/processor/QueryModifierPreProcessorTest.java
+++ src/test/java/de/ingrid/ibus/processor/QueryModifierPreProcessorTest.java
@@ -1,9 +1,6 @@
 package de.ingrid.ibus.processor;
 
 import junit.framework.TestCase;
-
-import org.apache.commons.configuration.PropertiesConfiguration;
-
 import de.ingrid.utils.query.IngridQuery;
 import de.ingrid.utils.queryparser.QueryStringParser;
 import de.ingrid.utils.tool.QueryUtil;
@@ -11,19 +8,9 @@ import de.ingrid.utils.tool.QueryUtil;
 public class QueryModifierPreProcessorTest extends TestCase {
 
     public void testProcess() throws Exception {
-        QueryModifierPreProcessor modifier = new QueryModifierPreProcessor();
+        QueryModifierPreProcessor modifier = new QueryModifierPreProcessor("/querymodifier.properties");
 
-        PropertiesConfiguration config = new PropertiesConfiguration();
-        config.addProperty("wasser", "wasser OR boden");
-        config.addProperty("cola", "fanta");
-        config.addProperty("drink:cola", "drink:fanta");
-        config.addProperty("cola", "fanta");
-        config.addProperty("nicht oft", "-oft");
-        config.addProperty("oft", "-(selten -nie)");
-
-        modifier.config = config;
-
-        IngridQuery q = QueryStringParser.parse("wasser cola bier (drink:cola OR drink:water -(oft \"nicht oft\"))");
+        IngridQuery q = QueryStringParser.parse("wasser Elbhochwasser cola bier (drink:cola OR drink:water -(oft \"nicht oft\"))");
 
         System.out.println("before: " + QueryUtil.query2String(q));
         modifier.process(q);
