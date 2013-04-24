cce8a5052bc68a899dfb2462ea1b71677ddadafe
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2013-04-24 08:06:54 +0000

    -adapt query modifier test case

--- src/test/java/de/ingrid/ibus/processor/QueryModifierPreProcessorTest.java
+++ src/test/java/de/ingrid/ibus/processor/QueryModifierPreProcessorTest.java
@@ -10,11 +10,20 @@ public class QueryModifierPreProcessorTest extends TestCase {
     public void testProcess() throws Exception {
         QueryModifierPreProcessor modifier = new QueryModifierPreProcessor("/querymodifier.properties");
 
-        IngridQuery q = QueryStringParser.parse("wasser Elbhochwasser cola bier (drink:cola OR drink:water -(oft \"nicht oft\"))");
+        IngridQuery q = QueryStringParser.parse("wasser Havelhochwasser cola bier (drink:cola OR drink:water -(oft \"nicht oft\"))");
 
         System.out.println("before: " + QueryUtil.query2String(q));
         modifier.process(q);
         System.out.println("after: " + QueryUtil.query2String(q));
+        
+        IngridQuery q1 = QueryStringParser.parse("Elbhochwasser OR (10 Jahre Elbehochwasser) OR (10 Jahre Elbe-Hochwasser) OR (Elbe-Hochwasser 2002) OR Elbehochwasser");
+        IngridQuery q2 = QueryStringParser.parse("Elbhochwasser");
+        modifier.process(q2);
+        System.out.println("query without modifier: " + QueryUtil.query2String(q1));
+        System.out.println("query with    modifier: " + QueryUtil.query2String(q2));
+        
+        
+        
     }
 
 }
