f4932aab1c88591530e0776f1017a06abadc2197
Andre Wallat <andre.wallat@wemove.com>
2016-10-19 16:16:06 +0200

    - fixed wrong path to save data when using cygwin (REDMINE-465)

--- src/main/release/scripts/start.sh
+++ src/main/release/scripts/start.sh
@@ -174,6 +174,7 @@ startIplug()
   # cygwin path translation
   if expr `uname` : 'CYGWIN*' > /dev/null; then
     CLASSPATH=`cygpath -p -w "$CLASSPATH"`
+    INGRID_HOME=`cygpath -p -w "$INGRID_HOME"`
   fi
 
   export CLASSPATH="$CLASSPATH"
