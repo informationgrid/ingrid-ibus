e4795f31db6323d9cb59d1e741fa14dc50ebfd02
André Wallat <andre.wallat@wemove.com>
2022-04-28 22:03:43 +0200

    Use hash value from indexed plugdescription when comparing for modification

--- backend/src/main/java/de/ingrid/ibus/comm/registry/Registry.java
+++ backend/src/main/java/de/ingrid/ibus/comm/registry/Registry.java
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.1 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl5
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -570,6 +570,7 @@ public class Registry {
             virtualPD = new PlugDescription();
 
 
+            virtualPD.setMd5Hash((String) pdFromIndex.get("md5Hash"));
             virtualPD.setProxyServiceURL((String) pdFromIndex.get("proxyServiceUrl"));
 
             List<String> datatypes = (List<String>) pdFromIndex.get("dataType");
