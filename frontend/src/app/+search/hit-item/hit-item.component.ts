f4015828dd6efb257bb58512d4de92c320bf1836
André Wallat <andre.wallat@wemove.com>
2022-08-03 22:15:26 +0200

    Fix click on detail link for SE results on iBus search page (#3129)

--- frontend/src/app/+search/hit-item/hit-item.component.ts
+++ frontend/src/app/+search/hit-item/hit-item.component.ts
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.1 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl5
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,11 +40,17 @@ export class HitItemComponent implements OnInit {
   }
 
   showDetailHit(hit: SearchHit) {
+    const hitId = hit.hitDetail['0'];
     if (hit.es_index) {
-      this.router.navigate(['/search/' + hit.es_index + '/' + hit.hitDetail['0']]);
+      const isWebsite = hitId.indexOf("http") === 0;
+      if (isWebsite) {
+        window.open(hitId, '_blank').focus();
+      } else {
+        this.router.navigate(['/search/' + hit.es_index + '/' + hitId]);
+      }
     } else {
       let plugIdEncoded = encodeURIComponent(hit.iPlugId);
-      this.router.navigate(['/search/' + plugIdEncoded + '/' + hit.hitDetail['0']], {
+      this.router.navigate(['/search/' + plugIdEncoded + '/' + hitId], {
         queryParams: {requestIPlug: true}
       });
     }
