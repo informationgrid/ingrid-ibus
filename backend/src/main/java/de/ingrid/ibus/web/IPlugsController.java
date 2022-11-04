0d8b7e495b02dfd6aab6eb7bd4aa363336b74e62
Jan Bücher <jan.buecher@wemove.com>
2022-11-04 14:52:17 +0100

    Update Dependencies Remove unused Import

--- backend/src/main/java/de/ingrid/ibus/web/IPlugsController.java
+++ backend/src/main/java/de/ingrid/ibus/web/IPlugsController.java
@@ -38,7 +38,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.websocket.server.PathParam;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.List;
