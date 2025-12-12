e83226f60d54e6d1c3d15abb0a8323d5e78dfce0
André Wallat <andre.wallat@wemove.com>
2025-12-12 11:09:58 +0100

    Update dependencies and configurations for frontend and backend
    - Update Angular dependencies to 21.0.5 and dev tools to their latest versions.
    - Bump Spring Framework version to 3.5.8.
    - Adjust `codelist-service.version` to `8.2.0-SNAPSHOT`.
    - Increment TypeScript version to 5.9.3.
    - Remove unnecessary `lib` configuration from `tsconfig.json`.
    
    (cherry picked from commit 1f720a980246573ef11d2296cac969e322f731a7)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.2 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * https://joinup.ec.europa.eu/software/page/eupl
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,12 +22,12 @@
  */
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {enableProdMode, provideZoneChangeDetection} from '@angular/core';
+import {platformBrowser} from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.log(err));
