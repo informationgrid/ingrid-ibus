b52900149e5e5e2c31a8f45b9b13520407c8b12e
André Wallat <andre.wallat@wemove.com>
2026-09-25 23:19:45 +0200

    Allow updating multiple ElasticSearch documents when toggling indices
    Harvester has multiple catalogs in one index, so these must be updated all in a meta-index

--- backend/src/main/java/de/ingrid/ibus/service/IndicesService.java
+++ backend/src/main/java/de/ingrid/ibus/service/IndicesService.java
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.2 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * https://joinup.ec.europa.eu/software/page/eupl
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -210,21 +210,22 @@ public class IndicesService {
             response = client.search(s -> s.index(INDEX_INFO_NAME)
                     .query(indexTypeQuery)
                     .source(fs -> fs.fetch(false))
-                    .size(1), ElasticDocument.class);
+                    .size(10), ElasticDocument.class);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
         List<Hit<ElasticDocument>> hits = response.hits().hits();
-        String id = hits.isEmpty() ? null : hits.get(0).id();
         Map<String, Object> jsonMap = new HashMap<>();
         jsonMap.put("active", active);
         try {
-            if (id != null) {
-                client.update(u -> u
-                        .index(INDEX_INFO_NAME)
-                        .id(id)
-                        .doc(jsonMap), ElasticDocument.class);
+            if (!hits.isEmpty()) {
+                for (Hit<ElasticDocument> hit : hits) {
+                    client.update(u -> u
+                            .index(INDEX_INFO_NAME)
+                            .id(hit.id())
+                            .doc(jsonMap), ElasticDocument.class);
+                }
             } else {
                 jsonMap.put(INDEX_FIELD_INDEX_ID, indexId);
                 client.index(c -> c
